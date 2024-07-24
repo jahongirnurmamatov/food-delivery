9161ec928636ff3d41ff7b6d0beafd6150756543
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-24 17:46:54 +0500

    addFood added

--- backend/controllers/foodController.js
+++ backend/controllers/foodController.js
@@ -4,7 +4,21 @@ import fs from 'fs';
 //ad food item
 
 const addFood =async(req,res)=>{
-
+    let image_filename = `${req.file.filename}`;
+    const food = new foodModel({
+        name:req.body.name,
+        price:req.body.price,
+        description:req.body.description,
+        category:req.body.category,
+        image:image_filename
+    });
+    try {
+        await food.save();
+        res.json({success:true,message:"Food added"})
+    } catch (error) {
+        console.log(error)
+        res.json({success:false,message:"Error:" ,error})
+    }
 }
 
 export {addFood};
