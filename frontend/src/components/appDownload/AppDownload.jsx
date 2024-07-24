5c3b45a602ed26199786cdb1a52e924b67f95a21
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-24 14:27:17 +0500

    responsives added to navbar, header, explore menu, and footer

--- frontend/src/components/appDownload/AppDownload.jsx
+++ frontend/src/components/appDownload/AppDownload.jsx
@@ -1,8 +1,15 @@
+import { assets } from '../../assets/assets'
 import './AppDownload.css'
 
 function AppDownload() {
   return (
-    <div>AppDownload</div>
+    <div className='app-download' id='app-download'>
+        <p>For Better Experience Download <br />Tomato App</p>
+        <div className="app-download-platforms">
+            <img src={assets.play_store} alt="" />
+            <img src={assets.app_store} alt="" />
+        </div>
+    </div>
   )
 }
 
