d319e59eb02f367bc34b5294f3a0b1af10a82ff7
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-24 14:37:24 +0500

    scrolling to navbar links added

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './Navbar.css'
+import {Link} from 'react-router-dom'
 import {assets} from '../../assets/assets'
 export default function Navbar() {
     const [menu, setMenu]=useState('home');
@@ -8,10 +9,10 @@ export default function Navbar() {
     <div className='navbar'>
         <img src={assets.logo} alt="" className='logo' />
         <ul className='navbar-menu'>
-            <li className={menu==='home'? "active":""} onClick={()=>setMenu('home')}>home</li>
-            <li className={menu==='menu'? "active":""} onClick={()=>setMenu('menu')}>menu</li>
-            <li className={menu==='mobile-app'? "active":""} onClick={()=>setMenu('mobile-app')}>mobile-app</li>
-            <li className={menu==='contact-us'? "active":""} onClick={()=>setMenu('contact-us')}>contact-us</li>
+            <Link to={'/'} className={menu==='home'? "active":""} onClick={()=>setMenu('home')}>home</Link>
+            <a href='#explore-menu' className={menu==='menu'? "active":""} onClick={()=>setMenu('menu')}>menu</a>
+            <a  href='#app-download' className={menu==='mobile-app'? "active":""} onClick={()=>setMenu('mobile-app')}>mobile-app</a>
+            <a  href='#footer' className={menu==='contact-us'? "active":""} onClick={()=>setMenu('contact-us')}>contact-us</a>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
