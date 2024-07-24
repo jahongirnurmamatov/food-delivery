dec682db4de624448466747657025ef6a36576d2
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-24 23:06:42 +0500

    addPage complete with toastify notifications

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,10 +5,12 @@ import {Routes ,Route } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import Orders from './pages/Orders/Orders'
 import List from './pages/List/List'
-
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   return (
     <div>
+      <ToastContainer />
       <Navbar/>
       <hr />
       <div className="app-content">
