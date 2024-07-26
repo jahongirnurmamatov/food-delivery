d5e7d85772678850a4db60ef12ac02b877af0590
jahongiir.nurmamatov <jahongiir.nurmamatov@gmail.com>
2024-07-26 18:43:05 +0500

    frontend complete, minor change to navbar and protect placeOrder page

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,10 +1,12 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import './PlaceOrder.css'
 import { StoreContext } from '../../ context/StoreContext'
 import { toast } from 'react-toastify';
 import axios from 'axios';
+import {useNavigate} from 'react-router-dom';
 function PlaceOrder() {
   const {getTotalCartAmount,token, food_list, cartItems,url} = useContext(StoreContext);
+  const navigate = useNavigate()
   const [data, setData] = useState({
     firstName:"",
     lastName:"",
@@ -45,6 +47,14 @@ function PlaceOrder() {
       toast.error(res.data.message);
     }
   }
+  
+  useEffect(()=>{
+    if(!token){
+      navigate('/cart');
+    }else if(getTotalCartAmount()===0){
+      navigate('/cart')
+    }
+  },[token])
 
 
   return (
